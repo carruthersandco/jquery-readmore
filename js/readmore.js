136f170f0d31844f16d9db31bd833c381adfd3a1
Carruthersandco <aimee.carruthers@gmail.com>
2016-09-09 23:34:02 +1000

    slide animation
    added slide up and slide down effects

--- js/readmore.js
+++ js/readmore.js
@@ -7,23 +7,22 @@ function start() {
 
 }
 
-
 function readMore(){
   event.preventDefault();
   $(".readmore").css("display", "none");
-  $("#show-this-on-click").show("slow");
+  $("#show-this-on-click").slideDown("slow");
   $(".readless").css("display", "block");
  }
 
 function readLess(){
   event.preventDefault();
   $(".readmore").css("display", "block");
-  $("#show-this-on-click").hide("slow");
+  $("#show-this-on-click").slideUp("slow");
   $(".readless").css("display", "none");
 }
 
 function learnMore(){
   event.preventDefault();
   $(".learnmore").css("display", "none");
-  $("#learnmoretext").show("slow");
+  $("#learnmoretext").slideDown("slow");
  }
